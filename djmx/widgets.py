29605e2e9722f51e179b78b6bea33a097f4f8e01
Eric Honkanen <dump@epicowl.com>
2013-07-18 14:51:09 -0400

    Add placeholder attr to widget render()

--- djmx/widgets.py
+++ djmx/widgets.py
@@ -1,7 +1,14 @@
 from mx.DateTime import DateTimeType
 
 from django.forms import widgets
+from django.forms.util import flatatt
 from django.contrib.admin import widgets as admin_widgets
+from django.utils.encoding import force_unicode
+from django.utils.safestring import mark_safe
+
+from mx import DateTime
+
+from .utils import era_format
 
 
 class DateInput(widgets.DateInput):
@@ -20,6 +27,19 @@ class DateInput(widgets.DateInput):
         if isinstance(value, DateTimeType):
             return value.date
         return value
+
+    def render(self, name, value, attrs=None):
+        if value is None:
+            value = ''
+        final_attrs = self.build_attrs(attrs, type=self.input_type, name=name)
+        if value != '':
+            # Only add the 'value' attribute if a value is non-empty.
+            final_attrs['value'] = force_unicode(self._format_value(value)) 
+            
+            # Add formatted value to attrs for display.
+            mxValue = DateTime.Parser.DateFromString(force_unicode(value))
+            final_attrs['placeholder'] = era_format(mxValue)
+        return mark_safe(u'<input%s />' % flatatt(final_attrs)) 
 
 
 class AdminDateWidget(DateInput, admin_widgets.AdminDateWidget):
